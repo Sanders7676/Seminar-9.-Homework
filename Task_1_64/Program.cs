b3a4abf656896fd339ac78f4e4528349ea7edf34
Sanders76 <sanders76@list.ru>
2023-01-04 17:57:56 +0300

    Added code to Task_1_64

--- Task_1_64/Program.cs
+++ Task_1_64/Program.cs
@@ -4,3 +4,51 @@
 
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+
+
+Console.WriteLine();
+
+Console.Write("Ведите число: ");
+
+int number = InputInt();
+
+Console.WriteLine();
+
+GetNatureNumbersNtoOne(number, 1);
+
+
+
+// Метод вывода чисел от N до 1
+
+void GetNatureNumbersNtoOne(int number, int finish = 1)
+{
+    if (number < finish)
+    {
+        return;
+    }
+    Console.Write($"{number} ");
+
+    GetNatureNumbersNtoOne(number - 1, finish);
+
+}
+
+
+
+// Метод для приема чисел, вводимых через консоль
+
+int InputInt()
+{
+    bool isNum = int.TryParse(Console.ReadLine(), out int num);
+
+    if (isNum)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное значение.");
+        return -1;
+    }
+}
