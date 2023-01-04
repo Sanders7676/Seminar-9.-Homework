920a53745f0c2cee43abfed0b0d53e001c7752d6
Sanders76 <sanders76@list.ru>
2023-01-04 20:21:33 +0300

    Added code to Task_2_66

--- Task_2_66/Program.cs
+++ Task_2_66/Program.cs
@@ -4,3 +4,69 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+
+
+Console.WriteLine();
+
+Console.Write("Ведите число M: ");
+
+int numberM = InputInt();
+
+Console.WriteLine();
+
+Console.Write("Ведите число N: ");
+
+int numberN = InputInt();
+
+Console.WriteLine();
+
+int SumFromNumberMToNumberN = GetSumFromNumberMToNumberN(numberM, numberN);
+
+Console.WriteLine(SumFromNumberMToNumberN);
+
+Console.WriteLine();
+
+
+
+// Метод вывода чисел от N до 1
+
+int GetSumFromNumberMToNumberN(int numberM, int numberN, int sum = 0, int count = 1)
+{
+    if (count <= (numberN - numberM))
+    {
+        sum = sum + numberM;
+
+        sum = GetSumFromNumberMToNumberN(numberM, numberN, sum + (numberM + 1), count + 1);
+    }
+    return sum;
+}
+
+
+
+// int GetSumFromNumberMToNumberN(int numberM, int numberN, int sum = 0, int count = 1)
+// {
+//     if (count <= (numberN - numberM))
+//     {
+//         sum = sum + GetSumFromNumberMToNumberN(numberM, numberN, sum + (numberM +1), count + 1);
+//     }
+//     return sum;
+// }
+
+
+// Метод для приема чисел, вводимых через консоль
+
+int InputInt()
+{
+    bool isNum = int.TryParse(Console.ReadLine(), out int num);
+
+    if (isNum)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное значение.");
+        return -1;
+    }
+}
