e3f1fd307f3f2b5a3e8190771fbe7e03fd4812ea
Sanders76 <sanders76@list.ru>
2023-01-05 12:01:48 +0300

    Added changes to code of Task_3_68

--- Task_3_68/Program.cs
+++ Task_3_68/Program.cs
@@ -3,3 +3,66 @@
 
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+
+
+Console.WriteLine();
+
+Console.Write("Ведите число M: ");
+
+int numberM = InputInt();
+
+Console.WriteLine();
+
+Console.Write("Ведите число N: ");
+
+int numberN = InputInt();
+
+Console.WriteLine();
+
+Console.WriteLine($"Значение Функции Аккермана A({numberM},{numberN}) равно: {GetValueAckermannFunction(numberM, numberN)}");
+
+Console.WriteLine();
+
+
+
+// Метод вычисления функции Аккермана
+
+int GetValueAckermannFunction (int numberM, int numberN)
+{
+    if (numberM == 0)
+    {
+        return numberN + 1;
+    }
+    
+    if (numberM != 0 && numberN == 0) 
+    {
+        return GetValueAckermannFunction(numberM - 1, 1);
+    }
+    
+    if (numberM > 0 && numberN > 0) 
+    {
+        return GetValueAckermannFunction(numberM - 1, GetValueAckermannFunction(numberM, numberN - 1));
+    }
+    
+    return GetValueAckermannFunction(numberM, numberN);
+}
+
+
+
+// Метод для приема чисел, вводимых через консоль
+
+int InputInt()
+{
+    bool isNum = int.TryParse(Console.ReadLine(), out int num);
+
+    if (isNum)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное значение.");
+        return -1;
+    }
+}
